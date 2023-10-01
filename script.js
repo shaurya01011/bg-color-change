63a1b0ddcb875d5733c778cd50f9d1be6360b34e
shaurya01011 <145752789+shaurya01011@users.noreply.github.com>
2023-10-01 18:58:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function myFunction() {
+    document.body.style.backgroundColor = "red";
+    document.getElementById("green").style.backgroundColor = "red";
+    document.getElementById("blue").style.backgroundColor = "red";
+    document.getElementById("green").style.color = "black";
+    document.getElementById("blue").style.color = "blue";
+    document.getElementById("red").style.backgroundColor = "red";
+    document.getElementById("red").style.color = "red";
+  }
+  function myFunction2() {
+    document.body.style.backgroundColor = "blue";
+    document.getElementById("red").style.backgroundColor = "blue";
+    document.getElementById("green").style.backgroundColor = "blue";
+    document.getElementById("red").style.color = "red";
+    document.getElementById("green").style.color = "black";
+    document.getElementById("blue").style.backgroundColor = "blue";
+    document.getElementById("blue").style.color = "blue";
+  }
+  function myFunction3() {
+    document.body.style.backgroundColor = "black";
+    document.getElementById("red").style.backgroundColor = "black";
+    document.getElementById("blue").style.backgroundColor = "black";
+    document.getElementById("red").style.color = "red";
+    document.getElementById("blue").style.color = "blue";
+    document.getElementById("green").style.backgroundColor = "black";
+    document.getElementById("green").style.color = "black";
+  }
